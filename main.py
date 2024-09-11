2005c55963bf0121bcd012b694c5b386ae1cb6f3
BairM1247 <mba84@mail.ru>
2024-09-11 22:41:35 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+first=(123)
+second=(234)
+third=(123)
+if first == second == third:
+    print (3)
+elif first == second or first == third or second == third:
+    print (2)
+elif first != second != third:
+    print(0)
+first = (23786)
+second = (23)
+third = (87)
+if first == second == third:
+    print (3)
+elif first == second or first == third or second == third:
+    print (2)
+elif first != second != third:
+    print(0)
